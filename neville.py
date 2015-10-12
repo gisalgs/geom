1568e2281f2a883b522a286c02134d568507f35b
Ningchuan Xiao <xiao.37@osu.edu>
2015-10-11 23:54:02 -0400

    Neville's algorithm for Robinson projection

---
+++ neville.py
@@ -0,0 +1,21 @@
+def neville(datax, datay, x):
+    """
+    Finds an interpolated value using Neville's algorithm.
+    Input
+      datax: input x's in a list of size n
+      datay: input y's in a list of size n
+      x: the x value used for interpolation
+    Output
+      p[0]: the polynomial of degree n
+    """
+    n = len(datax)
+    p = n*[0]                                     #*@\label{neville:sizen}
+    for k in range(n):
+        for i in range(n-k):
+            if k == 0:
+                p[i] = datay[i]
+	    else:
+                p[i] = ((x-datax[i+k])*p[i]+ \
+                        (datax[i]-x)*p[i+1])/ \
+                    (datax[i]-datax[i+k])         #*@\label{neville:pi}
+    return p[0]
