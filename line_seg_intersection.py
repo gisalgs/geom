d98fdfaa23ff1eae31b82b701faee2bff1de1ed0
Ningchuan Xiao <xiao.37@osu.edu>
2015-10-14 01:20:16 -0400

    line segments intersections

--- line_seg_intersection.py
+++ line_seg_intersection.py
@@ -1,3 +1,14 @@
+"""
+Line segment intersections using the Bentley-Ottmann Algorithm.
+
+Contact:
+Ningchuan Xiao
+The Ohio State University
+Columbus, OH
+"""
+
+__author__ = "Ningchuan Xiao <ncxiao@gmail.com>"
+
 from bintrees import AVLTree
 from point import *
 from intersection import *
@@ -21,7 +32,7 @@ def get_edges(t, p):
 
 def get_lr(T, s):
     """
-    Returns the left and right neighbors (branches) of p in T
+    Returns the left and right neighbors (branches) of s in T.
     """
     try:
         sl = T.floor_key(s)
@@ -46,6 +57,22 @@ def get_lrmost(T, segs):
     return l[0], l[-1]
 
 def find_new_event(s1, s2, p, q):
+    """
+    Tests if s1 intersects s2 at a point that is not in the event queue.
+    When a new intersection point is found, a new event will be created
+    and added to the event queue.
+
+    Input:
+       s1: line segment
+       s2: line segment
+       p: the point of the current event
+       q: event queue
+
+    Output:
+       True if a new point is found, False otherwise
+
+    Change: the content in the queue (q) may change.
+    """
     ip = intersectx(s1, s2)
     if ip is None:
         return False
@@ -60,8 +87,8 @@ def find_new_event(s1, s2, p, q):
 
 def intersectx(s1, s2):
     """
-    Tests intersection of 2 segments and returns the 
-    intersection point
+    Tests intersection of 2 input segments. If intersection is possible,
+    the actual intersection point will be calculated and returned.
     """
     if not test_intersect(s1, s2):
         return None
@@ -71,8 +98,10 @@ def intersectx(s1, s2):
 def intersections(psegs):
     """
     Implementation of the Bentley-Ottmann algorithm.
+
     Input
       psegs: a list of segments
+
     Output
       intpoints: a list of intersection points
     """
@@ -80,12 +109,12 @@ def intersections(psegs):
     intpoints = []
     T = AVLTree()
     L=[]
-    while not eq.is_empty():   # for all events
-        e = eq.events.pop(0)   # remove the event
-        p = e.p                # get event point
-        L = e.edges            # segments with p as left end
-        R,C = get_edges(T, p)  # p: right (R) and interior (C)
-        if len(L+R+C) > 1:     # Intersection at p among L+R+C
+    while not eq.is_empty():            # for all events
+        e = eq.events.pop(0)            # remove the event
+        p = e.p                         # get event point
+        L = e.edges                     # segments with p as left end
+        R,C = get_edges(T, p)           # p: right (R) and interior (C)
+        if len(L+R+C) > 1:              # Intersection at p among L+R+C
             for s in L+R+C:
                 if not s.contains(p):   # if p is interior
                     s.lp = p            # change lp and
@@ -105,11 +134,11 @@ def intersections(psegs):
             sp, spp = get_lrmost(T, L+C)
             try:
                 sl = T.prev_key(sp)
-            except KeyError:           # only on last key
+            except KeyError:            # only on last key
                 sl = None
             try:
                 sr = T.succ_key(spp)
-            except KeyError:           # only on last key
+            except KeyError:            # only on last key
                 sr = None
             find_new_event(sl, sp, p, eq)
             find_new_event(sr, spp, p, eq)
