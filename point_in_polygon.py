891e2386db94ac9c9fdf1eac0ba0ea6ba646968c
Ningchuan Xiao <xiao.37@osu.edu>
2016-10-06 22:39:53 -0400

    update point-in-polygon

--- point_in_polygon.py
+++ point_in_polygon.py
@@ -15,11 +15,12 @@ def pip_cross(point, pgon):
       of times the half line crosses the polygon boundary
 
     History
+       October 2016. pip_cross0 is removed; changed <> to !=
        October 2015. A bug in previous code, pip_cross0, is fixed.
     """
     tx, ty = point.x, point.y
     pgonx = pgon
-    if pgon[0] <> pgon[-1]:
+    if pgon[0] != pgon[-1]:
         pgonx.append(pgon[0])
     N = len(pgonx)
     crossing = 0
@@ -41,47 +42,6 @@ def pip_cross(point, pgon):
                     crossing += 1
                     inside_flag = not inside_flag
         yflag1 = yflag2
-    return inside_flag, crossing
-
-def pip_cross0(point, pgon):
-    """
-    Determines whether a point is in a polygon. Code adopted
-    from the C program in Graphics Gems IV by Haines (1994).
-
-    Input
-      pgon: a list of points as the vertices for a polygon
-      point: the point
-
-    Ouput
-      Returns a boolean value of True or False and the number
-      of times the half line crosses the polygon boundary
-    """
-    numvert = len(pgon)
-    tx=point.x
-    ty=point.y
-    p1 = pgon[numvert-1]                       # p1: first end point of edge
-    p2 = pgon[0]                               # p2: second end point of edge
-    yflag1 = (p1.y >= ty)                      # p1 on or above point
-    crossing = 0
-    inside_flag = 0
-    for j in range(numvert-1):
-        yflag2 = (p2.y >= ty)                  # p2 on or above point
-        if yflag1 != yflag2:                   # p1 & p2 on two sides of half line
-            xflag1 = (p1.x >= tx)              # left-right side of p1
-            xflag2 = (p2.x >= tx)              # left-right side of p2
-            if xflag1 == xflag2:               # p1 & p2 on same left/right side of point
-                if xflag1:
-                    crossing += 1
-                    inside_flag = not inside_flag
-            else:
-                m = p2.x - float((p2.y-ty))*\
-                    (p1.x-p2.x)/(p1.y-p2.y)    # compute intersection
-                if m >= tx:
-                    crossing += 1
-                    inside_flag = not inside_flag
-        yflag1 = yflag2
-        p1 = p2
-        p2 = pgon[j+1]
     return inside_flag, crossing
 
 if __name__ == "__main__":
