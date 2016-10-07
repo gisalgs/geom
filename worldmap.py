d0445a846bf092ea88f8c4891e3146b710ac54e5
Ningchuan Xiao <xiao.37@osu.edu>
2016-10-07 00:15:38 -0400

    update

--- worldmap.py
+++ worldmap.py
@@ -1,19 +1,27 @@
+"""
+Prepares data for projection transformation.
+
+History
+  October 6, 2016
+    Added default parameters for lat and lon bounds
+
+Contact:
+Ningchuan Xiao
+The Ohio State University
+Columbus, OH
+"""
 from osgeo import ogr
 
-def prep_projection_data(fname):
+def prep_projection_data(fname, lon0=-180, lon1=181, lat0=-90, lat1=91):
     points=[]
     linenum = 0
-    for lat in range(-90, 91, 10):
-        for lon in range(-180, 181, 10):
+    for lat in range(lat0, lat1, 10):
+        for lon in range(lon0, lon1, 10):
             points.append([linenum, lon, lat])
         linenum += 1
-    
-    for lon in range(-180, 181, 10):
-        for lat in range(-90, -80, 1):
-            points.append([linenum, lon, lat])
-        for lat in range(-80, 80, 10):
-            points.append([linenum, lon, lat])
-        for lat in range(80, 91, 1):
+
+    for lon in range(lon0, lon1, 10):
+        for lat in range(lat0, lat1, 10):
             points.append([linenum, lon, lat])
         linenum += 1
 
@@ -32,6 +40,6 @@ def prep_projection_data(fname):
             points.append([linenum, p[0], p[1]])
         linenum += 1
 
-    numline = max([p[0] for p in points]) + 1
+    # numline = max([p[0] for p in points]) + 1
 
-    return points, numgraticule, numline
+    return points, numgraticule, linenum
