a56bc8a58a86025668cd683bf702e07b1777c932
Ningchuan Xiao <xiao.37@osu.edu>
2017-10-12 10:48:25 -0400

    Robinson projection

--- transform1.py
+++ transform1.py
@@ -2,6 +2,7 @@
 A Python program for the Robinson projection.
 
 Contact:
+
 Ningchuan Xiao
 The Ohio State University
 Columbus, OH
@@ -13,7 +14,6 @@ import sys
 sys.path.append('..')
 from geom.neville import *
 import bisect
-# from numpy import fabs
 
 latitudes=[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60,
           65, 70, 75, 80, 85, 90]
