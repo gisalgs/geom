950aa2af436a44c852d2c5659ee03af0176f2495
Ningchuan Xiao <ncxiao@gmail.com>
2019-05-08 14:07:07 -0400

    side of point, left right

--- sideplr.py
+++ sideplr.py
@@ -1,4 +1,4 @@
-from point import *
+from .point import *
 
 def sideplr(p, p1, p2):
     """
@@ -17,13 +17,12 @@ if __name__ == "__main__":
     p=Point(1,1)
     p1=Point(0,0)
     p2=Point(1,0)
-    print "Point %s to line %s->%s: %d"%(
-        p, p1, p2, sideplr(p, p1, p2))
-    print "Point %s to line %s->%s: %d"%(
-        p, p2, p1, sideplr(p, p2, p1))
+    print("Point %s to line %s->%s: %d"%(
+        p, p1, p2, sideplr(p, p1, p2)))
+    print("Point %s to line %s->%s: %d"%(
+        p, p2, p1, sideplr(p, p2, p1)))
     p = Point(0.5, 0)
-    print "Point %s to line %s->%s: %d"%(
-        p, p1, p2, sideplr(p, p1, p2))
-    print "Point %s to line %s->%s: %d"%(
-        p, p2, p1, sideplr(p, p2, p1))
-
+    print("Point %s to line %s->%s: %d"%(
+        p, p1, p2, sideplr(p, p1, p2)))
+    print("Point %s to line %s->%s: %d"%(
+        p, p2, p1, sideplr(p, p2, p1)))
