7d966de2ac2f8ee472edf016665d72e93c6e272f
Ningchuan Xiao <ncxiao@gmail.com>
2020-10-17 00:35:01 -0400

    url geojson added

--- worldmap.py
+++ worldmap.py
@@ -2,6 +2,10 @@
 Prepares data for projection transformation.
 
 History
+    October 15, 2020
+        Added a new function prep_projection_data_url_geojson
+        to load geojson from a URL
+
     February 28, 2018
         Shapex is default
 
@@ -84,9 +88,39 @@ def prep_projection_data_shapex(fname, lon0=-180, lon1=181, lat0=-90, lat1=91):
 
     return points, numgraticule, linenum
 
+def prep_projection_data_url_geojson(url, lon0=-180, lon1=181, lat0=-90, lat1=91):
+    points=[]
+    linenum = 0
+    for lat in range(lat0, lat1, 10):
+        for lon in range(lon0, lon1, 10):
+            points.append([linenum, lon, lat])
+        linenum += 1
+
+    for lon in range(lon0, lon1, 10):
+        for lat in range(lat0, lat1, 10):
+            points.append([linenum, lon, lat])
+        linenum += 1
+
+    numgraticule = linenum
+
+    with request.urlopen(url) as response:
+        coastline_obj = json.loads(response.read())
+        
+    for f in coastline_obj['features']:
+        geom = f['geometry']['coordinates']
+        for p in geom[0]:
+            points.append([linenum, p[0], p[1]])
+        linenum += 1
+
+    return points, numgraticule, linenum
+
+
+
 def prep_projection_data(fname, lon0=-180, lon1=181, lat0=-90, lat1=91, _use_lib='SHAPEX'):
     if use_lib =='OGR' and _use_lib == 'OGR':
         return prep_projection_data_ogr(fname, lon0, lon1, lat0, lat1)
+    elif _use_lib='URL':
+        return prep_projection_data_url_geojson(fname, lon0, lon1, lat0, lat1)
     else:
         return prep_projection_data_shapex(fname, lon0, lon1, lat0, lat1)
 
